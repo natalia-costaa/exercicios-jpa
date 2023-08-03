06cf7436e7ba28bc50f6f789cfc1287cd5f4bbfb
natalia-costaa <ncalbuq@gmail.com>
2023-08-03 15:50:39 -0300

    Update ObterPedido.java

--- src/main/java/teste/umpramuitos/ObterPedido.java
+++ src/main/java/teste/umpramuitos/ObterPedido.java
@@ -11,13 +11,13 @@ public class ObterPedido {
 		DAO <Pedido> dao = new DAO<>(Pedido.class);
 		
 		Pedido pedido = dao.obterPorId(1L);
+		dao.fechar();
 		
 		for(ItemPedido item: pedido.getItens()) {
 			System.out.println(item.getQuantidade());
 			System.out.println(item.getProduto().getNome());
 		}
 		
-		dao.fechar();
 	}
 
 }
