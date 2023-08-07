369add94a85f8898558db9928e27251c15c6e491
natalia-costaa <ncalbuq@gmail.com>
2023-08-07 14:59:50 -0300

    Update NovoFilmeAtor.java

--- src/main/java/teste/muitospramuitos/NovoFilmeAtor.java
+++ src/main/java/teste/muitospramuitos/NovoFilmeAtor.java
@@ -8,8 +8,8 @@ public class NovoFilmeAtor {
 	
 	public static void main(String[] args) {
 		
-		Filme filmeA = new Filme("Star Wars Ep 4", 8.7);
-		Filme filmeB = new Filme("O Fugitivo", 8.9);
+		Filme filmeA = new Filme("Star Wars Ep 4", 8.9);
+		Filme filmeB = new Filme("O Fugitivo", 8.1);
 		
 		Ator atorA = new Ator("Harrison Ford");
 		Ator atrizB = new Ator("Carrie Fish");
@@ -19,7 +19,7 @@ public class NovoFilmeAtor {
 		
 		filmeB.adicionaAtor(atorA);
 		
-		DAO<Filme> dao = new DAO<>();
+		DAO<Filme> dao = new DAO<Filme>();
 		dao.incluirAtomico(filmeA);
 		
 	}
