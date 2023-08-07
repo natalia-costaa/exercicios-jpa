91e2cbe253566c96e5ef12100c782a2deb6a6d1f
natalia-costaa <ncalbuq@gmail.com>
2023-08-07 14:58:57 -0300

    Update DAO.java

--- src/main/java/infra/DAO.java
+++ src/main/java/infra/DAO.java
@@ -71,4 +71,19 @@ public class DAO<E>{
 		em.close();
 	}
 	
+	public List<E> consultar(String nomeConsulta, Object...params) {
+		TypedQuery<E> query = em.createNamedQuery(nomeConsulta, classe);
+		
+		for(int i = 0; i < params.length; i += 2) { //chave valor
+			query.setParameter(params[i].toString(), params[i+1]);
+		}
+		
+		return query.getResultList();
+	}
+	
+	public E consultarUm(String nomeConsulta, Object...params) {
+		List<E> lista = consultar(nomeConsulta, params);
+		return lista.isEmpty() ? null : lista.get(0);
+	}
+	
 }
